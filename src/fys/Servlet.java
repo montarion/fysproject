c9870289b57f4091c7f2a28906398b5316382124
Jamiro <jamirograntsaan@gmail.com>
2018-12-16 19:48:56 +0100

    iets meer logic toegevoegd

--- src/fys/Servlet.java
+++ src/fys/Servlet.java
@@ -5,11 +5,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-@WebServlet(name = "Servlet", urlPatterns = {"/index.html"}) //little bit of a hack. no need for extra url because browsers always look for index.html
+@WebServlet(name = "Servlet", urlPatterns = {"/index.html"})
+//little bit of a hack. no need for extra url because browsers always look for index.html
 public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -32,7 +47,7 @@ public class Servlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html lang=\"nl\">");
             out.println("<head>");
-            out.println("<title>WiFi in het vliegtuig | Corendon</title>");
+            out.println("<title>WiFi in het vliegtuig dsadsadsada| Corendon</title>");
             out.println("<meta charset=\"UTF-8\">");
             out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
             out.println("<link href=\"style/style.css\" type=\"text/css\" rel=\"stylesheet\">");
@@ -70,15 +85,123 @@ public class Servlet extends HttpServlet {
         }
     }
 
+    String firstName = "null";
+    String lastName = "null";
+    String ticketNumber = "";
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println("<script>");
+        String pattern = "(.*)=(.*)&(.*)=(.*)";
         String userdata = request.getReader().lines().collect(Collectors.joining());
-        out.println(String.format("console.log(\"%s\")", userdata));
-        out.println("</script>");
-        if (userdata.contains("admin")) {
-            out.println("<meta http-equiv=\"refresh\" content=\"0; URL='http://nu.nl'\" />"); //redirection example
+
+
+        //create search pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        //create matcher object
+        Matcher m = r.matcher(userdata);
+
+
+        if (m.find()) {
+            //ifirstName = m.group(2);
+            lastName = m.group(2); //4
+            ticketNumber = m.group(4); //6
+
+        } else {
+            System.out.println("NO MATCH");
         }
+        //verify ticket
+        String payload = Payloadbuilder("Ticket");
+        String APIresult = RequestAPI(payload, "Ticket");
+        System.out.println(APIresult);
+
+
+
+        // Laat ik erin als voorbeeld
+        out.println("<script>");
+        out.println(String.format("console.log(\"%s\")", APIresult));
+        out.println("</script>");
+
 
     }
+
+    public String Payloadbuilder(String choice) {
+        String payload = "";
+
+        if (choice.equals("Ticket")) {
+            payload = "{\n" +
+                    "\"function\": \"Check\"," +
+                    "\"teamId\": \"IC106-5\"," +
+                    "\"teamKey\": \"61d3f4b1959805acf5dafcd3dca7f7a6\"," +
+                    "\"requestId\" : \"1\"," +
+                    String.format("\"firstName\" : \"%s\",", firstName) +
+                    String.format("\"lastName\" : \"%s\",", lastName) +
+                    String.format("\"ticketNumber\" : \"%s\"", ticketNumber) +
+                    "}";
+        } else if (choice.equals("Passenger")) {
+
+            payload = "{\n" +
+                    "\"function\": \"List\",\n" +
+                    "\"teamId\": \"IC106-5\",\n" +
+                    "\"teamKey\": \"61d3f4b1959805acf5dafcd3dca7f7a6\",\n" +
+                    "\"requestId\" : \"1\"\n" +
+                    "}";
+        } else {
+            //flight payload
+            payload = "{" +
+                    "\"function\": \"List\"," +
+                    "\"teamId\": \"IC106-5\"," +
+                    "\"teamKey\": \"61d3f4b1959805acf5dafcd3dca7f7a6\"," +
+                    "\"requestId\" : \"1\"" +
+                    "}";
+        }
+
+    return payload;
 }
+
+    protected static String RequestAPI(String payload, String function) throws UnsupportedOperationException, IOException {
+        String result = null;
+        System.out.println(payload);
+        StringEntity entity = new StringEntity(payload,
+                ContentType.APPLICATION_FORM_URLENCODED);
+        System.out.println(payload);
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        String choice = function;
+        String url = String.format("http://fys.securidoc.nl:11111/%s", "Ticket");
+        HttpPost dbrequest = new HttpPost(url);
+        dbrequest.setEntity(entity);
+
+        HttpResponse response = httpClient.execute(dbrequest);
+        System.out.println(response.getStatusLine().getStatusCode());
+
+        HttpEntity entityresponse = response.getEntity();
+        if (entityresponse != null) {
+            InputStream instream = entityresponse.getContent();
+            result = convertStreamToString(instream);
+        }
+        return result;
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+}
+
